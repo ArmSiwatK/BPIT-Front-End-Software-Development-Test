379660850ac6a3095d83aeae7d04863b6b16bab9
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-19 13:48:13 +0700

    feat: Add Footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./Footer.scss";
+
+const copyrightText = "Copyright © HRD";
+const acknowledgements = "Acknowledgements";
+const miscellaneousLinks = [
+  { label: "Privacy Policy", url: "/privacy-policy" },
+  { label: "Terms of Service", url: "/terms-of-service" },
+  { label: "Contact Us", url: "/contact" },
+];
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      <div className="left">{copyrightText}</div>
+      <div className="middle">{acknowledgements}</div>
+      <div className="right">
+        {miscellaneousLinks.map((link, index) => (
+          <a key={index} href={link.url} className="link">
+            {link.label}
+          </a>
+        ))}
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
