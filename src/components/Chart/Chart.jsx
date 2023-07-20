691c07d52c792bc8b3192479eaf15db95f0d29f7
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-20 16:42:42 +0700

    feat: Color-coded background

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -18,26 +18,62 @@ const Chart = () => {
         siblings: 3
     }
 
-    const renderForeignObjectNode = ({
-        nodeDatum,
-        foreignObjectProps
-    }) => (
-        <g>
-            <foreignObject {...foreignObjectProps}>
-                <div className="chart-node">
-                    <div className="node-category">{nodeDatum.category}</div>
-                    <div className="node-name">{nodeDatum.name}</div>
-                    <div className="node-grade-container">
-                        <div className="node-grade">JOB GRADE: {nodeDatum.grade}</div>
-                        <div className="circle-button"><span className="plus-symbol">+</span></div>
+    const renderForeignObjectNode = ({ nodeDatum, foreignObjectProps }) => {
+        const applyGradeColor = (grade) => {
+            switch (grade) {
+                case "1":
+                    return '#444';
+                case "2":
+                    return '#666';
+                case "3":
+                    return '#888';
+                case "4":
+                    return '#aaa';
+                case "5":
+                    return '#ccc';
+                case "6":
+                    return '#eee';
+                default:
+                    return '#eee';
+            }
+        };
+
+        const nodeCategoryStyle = {
+            backgroundColor: applyGradeColor(nodeDatum.grade),
+        };
+
+        return (
+            <g>
+                <foreignObject {...foreignObjectProps}>
+                    <div className="chart-node">
+                        <div className="node-category" style={nodeCategoryStyle}>
+                            {nodeDatum.category}
+                        </div>
+                        <div className="node-name">{nodeDatum.name}</div>
+                        <div className="node-grade-container">
+                            <div className="node-grade">JOB GRADE: {nodeDatum.grade}</div>
+                            <div className="circle-button">
+                                <span className="plus-symbol">+</span>
+                            </div>
+                        </div>
                     </div>
-                </div>
-            </foreignObject>
-        </g>
-    );
+                </foreignObject>
+            </g>
+        );
+    };
+
 
     return (
         <div className="tree-wrapper" ref={containerRef}>
+            <div className="tree-backgrounds"></div>
+            <div className="job-grades">
+                <div className="grade-6">Job Grade 6</div>
+                <div className="grade-5">Job Grade 5</div>
+                <div className="grade-4">Job Grade 4</div>
+                <div className="grade-3">Job Grade 3</div>
+                <div className="grade-2">Job Grade 2</div>
+                <div className="grade-1">Job Grade 1</div>
+            </div>
             <Tree
                 data={CareerPath}
                 translate={translate}
